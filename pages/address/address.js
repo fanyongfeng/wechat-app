ac6b16de270418f40c509e69e46045686586a1a0
Yongfeng Fan <yongfeng@wemart.cn>
2018-05-27 16:43:07 +0800

    fix: remove host

--- pages/address/address.js
+++ pages/address/address.js
@@ -5,56 +5,174 @@ Page({
     dates1: '2017-01-01',
     dates2: '2017-02-14',
     dates3: '2017-03-07',
-    addDisabled: false,
-    greetingDisabled: true
+    switchData: [
+      {
+        id: 1,
+        isOn: false,
+        class: 'pull-right'
+      }, {
+        id: 2,
+        isOn: false,
+        class: 'pull-right'
+      }
+    ],
+    provinceList: [],
+    cityList: [],
+    receiveCustom: '',
+    mobileNo: '',
+    street: '',
+    provinceIndex: 0,
+    cityIndex: 0,
+    areaIndex: 0
+  },
+  //获取全国城市列表
+  onLoad: function () {
+    this.getProvince();
   },
   //CheckBoxEvent
-  checkboxChange: function(e) {
+  checkboxChange: function (e) {
     console.log('收货方提供地址', e.detail.value);
-    this.setData({
-      addDisabled: true
-    });
+    this.setData({addDisabled: true});
   },
   //DateEvent
-  bindDateChange1: function(e) {
-    this.setData({
-      dates1: e.detail.value
-    });
+  bindDateChange1: function (e) {
+    this.setData({dates1: e.detail.value});
   },
-  bindDateChange2: function(e) {
-    this.setData({
-      dates2: e.detail.value
-    });
+  bindDateChange2: function (e) {
+    this.setData({dates2: e.detail.value});
   },
-  bindDateChange3: function(e) {
-    this.setData({
-      dates3: e.detail.value
-    });
+  bindDateChange3: function (e) {
+    this.setData({dates3: e.detail.value});
   },
   //SwitchEvent
-  switch1Change: function(e) {
-    console.log('switch1 发生 change 事件，携带值为', e.detail.value);
-    this.setData({
-      greetingDisabled: false
-    });
-  },
-  switch2Change: function(e) {
-    console.log('switch2 发生 change 事件，携带值为', e.detail.value);
+  tapSwitch: function (event) {
+    var index = event.currentTarget.id - 1;
+    this.data.switchData[index].isOn = !this.data.switchData[index].isOn;
+    this.setData({switchData: this.data.switchData});
   },
   //PayButtonEvent
-  btnPay: function(e) {
+  btnPay: function (e) {
     app.selectTime = {
-        dates1: this.data.dates1,
-        dates2: this.data.dates2,
-        dates3: this.data.dates3, 
+      dates1: this.data.dates1,
+      dates2: this.data.dates2,
+      dates3: this.data.dates3
     };
-    wx.showModal({
-        title: '错误提示',
-        content: '请填写地址'
-    });
+    var data = this.data;
+    app.receiveDetail = {
+      address: data.provinceList[data.provinceIndex] + data.cityList[data.cityIndex].name + data.areaList[data.areaIndex],
+      receiveCustom: data.receiveCustom,
+      mobileNo: data.mobileNo,
+      street: data.street
+    };
+    if (!app.receiveDetail.address || !app.receiveDetail.mobileNo || !app.receiveDetail.street) {
+      wx.showModal({title: '错误提示', content: '请讲收获信息填写完整'});
+    }
     // return false;
-    wx.navigateTo({
-      url: '../confirm/confirm'
+    wx.navigateTo({url: '../confirm/confirm'});
+  },
+  getProvince: function () {
+    var self = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: '/api/configmng/geo',
+      method: 'GET',
+      dataType: 'json',
+      success: function (res) {
+        self.renderProvince(res && res.data);
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+      complete: wx.hideNavigationBarLoading
     });
+  },
+  bindProvinceChange: function (e) {
+    this.setData({provinceIndex: e.detail.value});
+    this.getCityList(this.data.provinceList[e.detail.value]);
+  },
+  renderProvince: function (res) {
+    console.log(res);
+    var provinceList = [];
+    var data = res.data;
+    if (data) {
+      for (var index in data) {
+        provinceList.push(data[index].name);
+      }
+      this.setData({provinceList: provinceList});
+      this.getCityList(provinceList[0]);
+    }
+  },
+  getCityList: function (provinceName) {
+    var self = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: '/api/configmng/geo',
+      method: 'GET',
+      dataType: 'json',
+      data: {
+        para: JSON.stringify({provinceName: provinceName})
+      },
+      success: function (res) {
+        self.renderCity(res && res.data);
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+      complete: wx.hideNavigationBarLoading
+    });
+  },
+  renderCity: function (res) {
+    console.log(res);
+    var data = res.data;
+    if (data) {
+      this.setData({cityList: data});
+      this.getAreaList(data[0].cityNo);
+    }
+  },
+  bindCityChange: function (e) {
+    this.setData({cityIndex: e.detail.value});
+    this.getAreaList(this.data.cityList[e.detail.value].cityNo);
+  },
+  getAreaList: function (cityNo) {
+    var self = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: '/api/configmng/geo',
+      method: 'GET',
+      dataType: 'json',
+      data: {
+        para: JSON.stringify({cityNo: cityNo})
+      },
+      success: function (res) {
+        self.renderArea(res && res.data);
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+      complete: wx.hideNavigationBarLoading
+    });
+  },
+  renderArea: function (res) {
+    console.log(res);
+    var areaList = [];
+    var data = res.data;
+    if (data) {
+      for (var index in data) {
+        areaList.push(data[index].name);
+      }
+      this.setData({areaList: areaList});
+    }
+  },
+  bindAreaChange: function (e) {
+    this.setData({areaIndex: e.detail.value});
+  },
+  receiveCustomChange: function (e) {
+    this.setData({receiveCustom: e.detail.value});
+  },
+  mobileNoChange: function (e) {
+    this.setData({mobileNo: e.detail.value});
+  },
+  streetChange: function (e) {
+    this.setData({street: e.detail.value});
   }
 });
--- pages/confirm/confirm.js
+++ pages/confirm/confirm.js
@@ -6,16 +6,118 @@ Page({
     address_detail: '北京市海淀区113号5单元121号',
     dates1: '2017-01-01',
     dates2: '2017-02-14',
-    dates3: '2017-03-07',
+    dates3: '2017-03-07'
   },
-  onReady: function() {
+  onReady: function () {
     this.setData({
       dates1: app.selectTime.dates1,
       dates2: app.selectTime.dates2,
       dates3: app.selectTime.dates3,
+      receive_name: app.receiveDetail.receiveCustom,
+      address_detail: app.receiveDetail.address + app.receiveDetail.street
+    });
+    wx.request({
+      url: '/api/shopping/buyer/doauth/',
+      method: 'post',
+      datatype: 'json',
+      data: {
+        para: {
+          "redirect": "",
+          "sellerId": "286",
+          "channelId": "839"
+        }
+      },
+      success: function (res) {
+        debugger
+      },
+      fail: function (err) {
+        console.log(err);
+      }
     });
   },
-  goback: function() {
+  goback: function () {
     wx.navigateBack();
+  },
+  payByself: function () {
+    var self = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: '/api/shopping/order/create',
+      method: 'post',
+      datatype: 'json',
+      data: {
+        para: {
+          "order": [{
+            "commSellerId": "228",
+            "fareMoney": 1,
+            "goods": [{
+              "barCode": "",
+              "buyVol": 1,
+              "commSkuId": "sku98703",
+              "commsellerId": "228",
+              "fareMoney": 1,
+              "goodsId": "gd28872",
+              "goodsName": "房改房【江浙沪1山东2全国3新疆4】",
+              "isFarefree": 0,
+              "limitVol": 0,
+              "moneyUnit": "CNY",
+              "picUrl": "http://imgcache9.img.cn/FukkMqckYshguBj_mfjv8CV1D-7q",
+              "retailPrice": 1,
+              "skuContent": "测试:1",
+              "skuId": "sku98703",
+              "stocks": 20,
+              "supplyPrice": 1,
+              "supplySellerId": "228",
+              "supplySkuId": "sku98703",
+              "retailPriceTxt": "0.01"
+            }],
+            "goodsNum": 1,
+            "groupName": "发货1",
+            "remarkChecked": 0,
+            "remarkContent": "选填，可填写您和卖家达成一致的要求",
+            "supplySellerId": "228",
+            "totalPrice": 2,
+            "totalPriceTxt": "0.02"
+          }],
+          "marketing": [{
+            "type": "loyaltyPoints",
+            "value": 0
+          }, {
+            "type": "coupon",
+            "value": 0
+          }, {
+            "type": "couponcode",
+            "value": 0
+          }],
+          "payTypeList": [{
+            "payType": 1,
+            "paySubType": 2
+          }],
+          "receive": {
+            "addrNo": 36759,
+            "cityNo": 73,
+            "receiver": "樊勇锋",
+            "receiverAddr": "上海市_上海市_嘉定区_舍得放手",
+            "receiverPhone": "18616386349"
+          },
+          "buyerComment": {
+            "228_228": {
+              "remarkComment": ""
+            }
+          },
+          "info": {
+            "entryId": ""
+          },
+          "returnUrl": ""
+        }
+      },
+      success: function (res) {
+        debugger
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+      complete: wx.hideNavigationBarLoading
+    });
   }
 });
